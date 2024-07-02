d8782aab58187e035a02acba4d9703f3c96638c8
Jayalakshmi.M <jayalakshmim720@gmail.com>
2024-07-02 18:02:37 +0530

    second commit

---
+++ doubleylinkedlist.py
@@ -0,0 +1,42 @@
+###########################################creation of doublylinkedlist and reverse#############################################
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+class DlinkedL:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    def addnode(self,key):
+        newnode=Node(key)
+        if self.head is None:
+            self.head=newnode
+        else:
+            self.tail.next=newnode
+            newnode.prev=self.tail
+        self.tail=newnode
+    def display(self):
+        if self.head is None:
+            print("list is empty")
+        temp=self.head
+        while(temp is not None):
+            print(temp.data)
+            temp=temp.next
+    def reverse(self):
+        if self.head is None:
+            return
+        temp=self.tail
+        while(temp is not None):
+            print(temp.data)
+            temp=temp.prev
+list=DlinkedL()
+list.display()
+arr=[5,4,3,2,1]
+for i in arr:
+    list.addnode(i)
+list.display()
+print("reversed")
+list.reverse()
+
